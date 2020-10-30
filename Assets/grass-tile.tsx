0b9d0f271dda2186963dd7d7ab1889b36696f939
KonradRundfunk <konrad@eichsfeld.net>
2020-10-30 17:59:23 +0100

    very experimental
    added new tiles for testing

---
+++ Assets/grass-tile.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="grass-tile" tilewidth="32" tileheight="32" tilecount="1" columns="1">
+ <image source="grass-tile.png" width="48" height="48"/>
+</tileset>
