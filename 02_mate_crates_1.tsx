f46aa1bc7b59d335231004aea09bd6928fbc8ee7
fingadumbledore <fingadumbledore@gmail.com>
2021-11-16 17:44:06 +0100

    new file:   01_mate.tsx 	new file:   02_mate_crates_1.tsx 	new file:   03_latop_sticker_32x32t.tsx 	new file:   05_caution_biohazard.tsx 	new file:   05_mate_bottles_full.tsx 	new file:   06_wash_your_hands_32x32.tsx 	new file:   07_wear_your_mask_32x32.tsx 	new file:   52_wallscreen1.png 	new file:   52_wallscreen1.tsx 	new file:   53_wallscreen2.png 	new file:   53_wallscreen2.tsx 	new file:   54_wallscreen3.png 	new file:   54_wallscreen3.tsx 	new file:   Bowser.png 	new file:   Bowser.tsx 	new file:   Outside Objects.tsx 	new file:   Princess_Peach.png 	new file:   Princess_Peach.tsx 	new file:   Super_Mario.png 	new file:   Super_Mario.tsx 	new file:   animales2.png 	new file:   animales2.tsx 	new file:   aquarium1.png 	new file:   aquarium1.tsx 	new file:   brandy_rockets.tsx 	new file:   dungeon.png 	new file:   dungeon.tsx 	new file:   fische.png 	new file:   fische.tsx 	new file:   fische2-improved.png 	new file:   fische2-improved.tsx 	new file:   fische2.png 	new file:   fische2.tsx 	modified:   keller.json 	modified:   main.json 	new file:   rc3-mullana-colors-128.png 	new file:   rc3-mullana-colors-128.tsx 	new file:   rc3-mullana-functionary.tsx 	new file:   rc3-mullana-neon-signs.tsx 	new file:   recycle_items.png 	new file:   recycle_items.tsx 	new file:   see.tmx 	new file:   watergrass.tsx

---
+++ 02_mate_crates_1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="02_mate_crates_1" tilewidth="32" tileheight="32" tilecount="3" columns="1">
+ <image source="../../world-map-starterkit/tiles_png/community/cert/imgs/food_and_drinks/02_mate_crates_1.png" width="32" height="96"/>
+</tileset>
---
+++ brandy_rockets.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="brandy_rockets" tilewidth="32" tileheight="32" tilecount="1980" columns="60">
+ <image source="brandy_rockets.png" width="1920" height="1080"/>
+</tileset>
