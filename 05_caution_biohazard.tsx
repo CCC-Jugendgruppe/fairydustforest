ce9667c70a8c89ab7977aaa4d41fe3b7ab6ac729
Fingadumbledore <53864355+Fingadumbledore@users.noreply.github.com>
2021-12-04 10:18:29 +0100

    Delete 05_caution_biohazard.tsx

--- 05_caution_biohazard.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="05_caution_biohazard" tilewidth="32" tileheight="32" tilecount="1" columns="1">
- <image source="../../world-map-starterkit/tiles_png/community/cert/imgs/c3gelb/05_caution_biohazard.png" width="32" height="32"/>
-</tileset>
