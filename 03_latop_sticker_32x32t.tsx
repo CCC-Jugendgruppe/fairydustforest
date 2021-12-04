341ac0f9ed18c6419b29e71ece241ef645da07a0
Fingadumbledore <53864355+Fingadumbledore@users.noreply.github.com>
2021-12-04 10:18:23 +0100

    Delete 03_latop_sticker_32x32t.tsx

--- 03_latop_sticker_32x32t.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="03_latop_sticker_32x32t" tilewidth="32" tileheight="32" tilecount="1" columns="1">
- <image source="../../world-map-starterkit/tiles_png/community/cert/imgs/misc/03_latop_sticker_32x32t.png" width="32" height="32"/>
-</tileset>
