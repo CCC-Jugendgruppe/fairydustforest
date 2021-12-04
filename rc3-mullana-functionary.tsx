6dbfee38bd430ee7b73520c0d94ea7089f0fc773
Fingadumbledore <53864355+Fingadumbledore@users.noreply.github.com>
2021-12-04 09:23:25 +0000

    Deploying to gh-pages from master @ de1238666d152b86b18d93994c7993ace5d6d626 🚀

--- rc3-mullana-functionary.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="rc3-mullana-functionary" tilewidth="32" tileheight="32" tilecount="256" columns="16">
- <image source="../../world-map-starterkit/tiles_png/structures/rc3-mullana-functionary.png" width="512" height="512"/>
-</tileset>
